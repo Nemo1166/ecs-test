80f4318e8d3860a60e1f4ea9de6ebae229c35b7f
Nemo1166 <zhangguoxi1166@gmail.com>
2024-12-01 12:48:43 +0000

    convert space to tab for indent

--- scripts/ecs/ecs.gd
+++ scripts/ecs/ecs.gd
@@ -2,108 +2,108 @@ class_name ECS extends RefCounted
 
 
 class World extends RefCounted:
-    var _name: String = ""
-    var ent_mgr: EntityManager = EntityManager.new(self)
-    var systems: Array[System] = []
+	var _name: String = ""
+	var ent_mgr: EntityManager = EntityManager.new(self)
+	var systems: Array[System] = []
 
-    var debug_mode: bool = false
+	var debug_mode: bool = false
 
-    func _init(name: String) -> void:
-        _name = name
+	func _init(name: String) -> void:
+		_name = name
 
-    ## 添加系统
-    func add_system(system: System) -> void:
-        systems.append(system)
+	## 添加系统
+	func add_system(system: System) -> void:
+		systems.append(system)
 
-    ## 移除系统
-    func remove_system(system: System) -> void:
-        if systems.has(system):
-            systems.erase(system)
+	## 移除系统
+	func remove_system(system: System) -> void:
+		if systems.has(system):
+			systems.erase(system)
 
-    ## 更新系统
-    func update(delta: float) -> void:
-        for system in systems:
-            system.update(delta)
+	## 更新系统
+	func update(delta: float) -> void:
+		for system in systems:
+			system.update(delta)
 
-    ## 清空世界
-    func clear() -> void:
-        ent_mgr.entities.clear()
-        systems.clear()
+	## 清空世界
+	func clear() -> void:
+		ent_mgr.entities.clear()
+		systems.clear()
 
 class Entity extends RefCounted:
-    var _entity_id: int = 0
-    var tags: Array = []
-    var components: Dictionary = {}
+	var _entity_id: int = 0
+	var tags: Array = []
+	var components: Dictionary = {}
 
-    func _init(_id) -> void:
-        _entity_id = _id
+	func _init(_id) -> void:
+		_entity_id = _id
 
 
 class EntityManager extends RefCounted:
-    var _world: World = null
-    var _entity_id: int = 0
-    var entities: Dictionary = {}
+	var _world: World = null
+	var _entity_id: int = 0
+	var entities: Dictionary = {}
 
-    func _init(world: World) -> void:
-        _world = world
+	func _init(world: World) -> void:
+		_world = world
 
-    ## 创建实体
-    func create_entity() -> Entity:
-        var ent = Entity.new(_entity_id)
-        entities[_entity_id] = ent
-        _entity_id += 1
-        return ent
+	## 创建实体
+	func create_entity() -> Entity:
+		var ent = Entity.new(_entity_id)
+		entities[_entity_id] = ent
+		_entity_id += 1
+		return ent
 
-    ## 移除实体
-    func remove_entity(ent_id: int) -> bool:
-        if entities.has(ent_id):
-            entities.erase(ent_id)
-            return true
-        return false
+	## 移除实体
+	func remove_entity(ent_id: int) -> bool:
+		if entities.has(ent_id):
+			entities.erase(ent_id)
+			return true
+		return false
 
-    ## 获取实体
-    func get_entity(ent_id: int) -> Entity:
-        if entities.has(ent_id):
-            return entities[ent_id]
-        return null
+	## 获取实体
+	func get_entity(ent_id: int) -> Entity:
+		if entities.has(ent_id):
+			return entities[ent_id]
+		return null
 
-    ## 添加组件
-    func add_component(ent_id: int, comp_name: String, comp: Component) -> void:
-        if entities.has(ent_id):
-            entities[ent_id].components[comp_name] = comp
+	## 添加组件
+	func add_component(ent_id: int, comp_name: String, comp: Component) -> void:
+		if entities.has(ent_id):
+			entities[ent_id].components[comp_name] = comp
 
-    ## 移除组件
-    func remove_component(ent_id: int, comp_name: String) -> void:
-        if entities.has(ent_id):
-            if entities[ent_id].components.has(comp_name):
-                entities[ent_id].components.erase(comp_name)
+	## 移除组件
+	func remove_component(ent_id: int, comp_name: String) -> void:
+		if entities.has(ent_id):
+			if entities[ent_id].components.has(comp_name):
+				entities[ent_id].components.erase(comp_name)
 
-    ## 获取组件
-    func get_component(ent_id: int, comp_name: String) -> Component:
-        if entities.has(ent_id):
-            if entities[ent_id].components.has(comp_name):
-                return entities[ent_id].components[comp_name]
-        return null
+	## 获取组件
+	func get_component(ent_id: int, comp_name: String) -> Component:
+		if entities.has(ent_id):
+			if entities[ent_id].components.has(comp_name):
+				return entities[ent_id].components[comp_name]
+		return null
 
-    ## 获取所有实体
-    func get_all_entity_ids() -> Array[int]:
-        return entities.keys()
+	## 获取所有实体
+	func get_all_entity_ids() -> Array[int]:
+		return entities.keys()
 
 
 class System extends RefCounted:
-    var _world: World = null
+	var _world: World = null
 
-    func _init(world: World) -> void:
-        _world = world
-    # override
-    func update(_delta: float) -> void:
-        pass
-    
-    func get_all_entities() -> Array[Entity]:
-        return _world.entities.values()
+	func _init(world: World) -> void:
+		_world = world
+	# override
+	func update(_delta: float) -> void:
+		pass
+	
+	func get_all_entities() -> Array[Entity]:
+		return _world.entities.values()
 
 
 class Component extends RefCounted:
-    func _init() -> void:
-        pass
+	func _init() -> void:
+		pass
 
--- scripts/ecs/system/production_sys.gd
+++ scripts/ecs/system/production_sys.gd
@@ -5,56 +5,56 @@ var global_efficiency: float = 1.0
 
 
 func _init(world: ECS.World) -> void:
-    super._init(world)
+	super._init(world)
 
 
 func update(delta: float) -> void:
-    var ent_ids = _world.ent_mgr.get_all_entity_ids()
-    for ent_id in ent_ids:
-        var producer: Comps.Producer = _world.ent_mgr.get_component(ent_id, "Producer")
-        var input: Comps.Inventory = _world.ent_mgr.get_component(ent_id, "InputDepot")
-        var output: Comps.Inventory = _world.ent_mgr.get_component(ent_id, "OutputDepot")
-        if producer != null:
-            if producer.recipe != null:
-                # check if enough resources
-                if not has_enough_resources(input, producer.recipe):
-                    producer.work_state = Comps.Producer.WorkState.INSUFFICIENT_RESOURCES
-                    producer.current_state = Comps.Producer.State.IDLE
-                    continue
-                # check if too many products
-                elif too_many_products(output, producer.recipe):
-                    producer.work_state = Comps.Producer.WorkState.INSUFFICIENT_SPACE
-                    producer.current_state = Comps.Producer.State.IDLE
-                    continue
-                else:
-                    producer.work_state = Comps.Producer.WorkState.NORMAL
-                    producer.current_state = Comps.Producer.State.WORKING
-                # produce products
-                producer.progress += delta * global_efficiency
-                if producer.progress >= 100:
-                    # collect products
-                    producer.progress = 0
-                    for i in producer.recipe.products.keys():
-                        var amount: float = producer.recipe.results[i]
-                        output.add_item(i, amount)
-                    # consume resources
-                    for i in producer.recipe.ingredients.keys():
-                        var amount: float = producer.recipe.ingredients[i]
-                        input.remove_item(i, amount)
+	var ent_ids = _world.ent_mgr.get_all_entity_ids()
+	for ent_id in ent_ids:
+		var producer: Comps.Producer = _world.ent_mgr.get_component(ent_id, "Producer")
+		var input: Comps.Inventory = _world.ent_mgr.get_component(ent_id, "InputDepot")
+		var output: Comps.Inventory = _world.ent_mgr.get_component(ent_id, "OutputDepot")
+		if producer != null:
+			if producer.recipe != null:
+				# check if enough resources
+				if not has_enough_resources(input, producer.recipe):
+					producer.work_state = Comps.Producer.WorkState.INSUFFICIENT_RESOURCES
+					producer.current_state = Comps.Producer.State.IDLE
+					continue
+				# check if too many products
+				elif too_many_products(output, producer.recipe):
+					producer.work_state = Comps.Producer.WorkState.INSUFFICIENT_SPACE
+					producer.current_state = Comps.Producer.State.IDLE
+					continue
+				else:
+					producer.work_state = Comps.Producer.WorkState.NORMAL
+					producer.current_state = Comps.Producer.State.WORKING
+				# produce products
+				producer.progress += delta * global_efficiency
+				if producer.progress >= 100:
+					# collect products
+					producer.progress = 0
+					for i in producer.recipe.products.keys():
+						var amount: float = producer.recipe.results[i]
+						output.add_item(i, amount)
+					# consume resources
+					for i in producer.recipe.ingredients.keys():
+						var amount: float = producer.recipe.ingredients[i]
+						input.remove_item(i, amount)
 
 
 func has_enough_resources(depot: Comps.Inventory, recipe: Recipe) -> bool:
-    for i in recipe.ingredients.keys():
-        var amount: float = recipe.ingredients[i]
-        if not depot.has_item(i, amount):
-            return false
-    return true
+	for i in recipe.ingredients.keys():
+		var amount: float = recipe.ingredients[i]
+		if not depot.has_item(i, amount):
+			return false
+	return true
 
 
 func too_many_products(depot: Comps.Inventory, recipe: Recipe) -> bool:
-    const PRODUCT_COEFFICIENT := 2
-    for i in recipe.products.keys():
-        var amount: float = recipe.results[i] * PRODUCT_COEFFICIENT
-        if depot.has_item(i, amount):
-            return true
-    return false
+	const PRODUCT_COEFFICIENT := 2
+	for i in recipe.products.keys():
+		var amount: float = recipe.results[i] * PRODUCT_COEFFICIENT
+		if depot.has_item(i, amount):
+			return true
+	return false
